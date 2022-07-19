bc64b1642fca270d9d97117533d5592a322fb57e
FATMA KOÇAK <42037891+fatmakocak@users.noreply.github.com>
2022-07-19 16:44:00 +0300

    Delete src directory

--- src/main/java/com/deneme/employeemanager/exception/userNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.deneme.employeemanager.exception;
-
-public class userNotFoundException extends RuntimeException{
-    public userNotFoundException(String message) {
-    super(message);
-    }
-}
